971606e4a219f82ce2aeb733d58a0bfa7ebfe695
nmimran99 <nmimran99@gmail.com>
2023-08-15 23:37:51 +0300

    comm

--- components/Whatsapp.jsx
+++ components/Whatsapp.jsx
@@ -6,7 +6,7 @@ export default function Whatsapp() {
         <div className="fixed bottom-5 left-5 z-50">
             <a target="_blank" rel="noopener noreferrer" href="https://api.whatsapp.com/send?phone=972526267419">
                 <Image
-                    src="/icons/Whatsapp.svg"
+                    src="/icons/whatsapp.svg"
                     alt="whatsappIcon"
                     height="60"
                     width="60"
